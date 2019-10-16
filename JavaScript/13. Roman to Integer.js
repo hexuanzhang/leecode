3d3e7acbff0a555f713e23610f48a81d7ee335ff
hexuanzhang <136789658@qq.com>
2019-10-16 23:48:55 +0800

    13. Roman to Integer v1

---
+++ JavaScript/13. Roman to Integer.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ * @Address: https://leetcode.com/problems/roman-to-integer/ | https://leetcode-cn.com/problems/roman-to-integer/
+ * @Author: hexuan.zhang
+ * @Date: 2019-10-16 22:53:31
+ * @Last Modified by: hexuan.zhang
+ * @Last Modified time: 2019-10-16 23:48:29
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const romanToInt = (s) => {
+
+}
