574d0b0ca4deaa9f1b6c6b698f9fcec4b75e61dc
hexuanzhang <136789658@qq.com>
2019-10-17 23:00:42 +0800

    13. Roman to Integer v2

--- JavaScript/13. Roman to Integer.js
+++ JavaScript/13. Roman to Integer.js
@@ -12,5 +12,62 @@
  * @return {number}
  */
 const romanToInt = (s) => {
+    const ROMAN_MAP = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    };
 
+    let result = 0;
+
+    let index = 0;
+    while (index < s.length) {
+        let chart = s[index];
+
+        switch (chart) {
+            case 'C':
+                if (['M', 'D'].includes(s[index + 1])) { // 'CM'、'CD'
+                    result += ROMAN_MAP[`C${s[index + 1]}`];
+                    index += 2;
+                } else { // 'C'
+                    result += ROMAN_MAP[chart];
+                    ++index;
+                }
+                break;
+            case 'X':
+                if (['L', 'C'].includes(s[index + 1])) { // 'XL'、'XC'
+                    result += ROMAN_MAP[`X${s[index + 1]}`];
+                    index += 2;
+                } else { // 'X'
+                    result += ROMAN_MAP[chart];
+                    ++index;
+                }
+                break;
+            case 'I':
+                if (['X', 'V'].includes(s[index + 1])) { // 'IX'、'IV'
+                    result += ROMAN_MAP[`I${s[index + 1]}`];
+                    index += 2;
+                } else { // 'I'
+                    result += ROMAN_MAP[chart];
+                    ++index;
+                }
+                break;
+            default:
+                result += ROMAN_MAP[chart];
+                ++index;
+
+        }
+    }
+
+    return result;
 }
