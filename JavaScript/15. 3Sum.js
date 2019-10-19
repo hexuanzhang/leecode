2b2b9ff4b906f16216481b328dabf32b9372cf36
hexuanzhang <136789658@qq.com>
2019-10-19 22:59:11 +0800

    15. 3Sum v1

---
+++ JavaScript/15. 3Sum.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0; 如果存在，则找出所有满足条件且不重复的三元组
+ * @Address: https://leetcode.com/problems/3sum/ | https://leetcode-cn.com/problems/3sum/
+ * @Author: hexuan.zhang
+ * @Date: 2019-10-19 22:56:25
+ * @Last Modified by: hexuan.zhang
+ * @Last Modified time: 2019-10-19 22:58:47
+ */
+
+/**
+* @param {number[]} nums
+* @return {number[][]}
+*/
+const threeSum = (nums) => {
+
+};
