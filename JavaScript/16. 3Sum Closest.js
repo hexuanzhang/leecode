3c575ff5a95861224dae236b908875d870b13697
hexuanzhang <136789658@qq.com>
2019-10-23 00:14:12 +0800

    16. 3Sum Closest v1

---
+++ JavaScript/16. 3Sum Closest.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * @Address：https://leetcode.com/problems/3sum-closest/ | https://leetcode-cn.com/problems/3sum-closest/
+ * @Author: hexuan.zhang
+ * @Date: 2019-10-22 23:49:16
+ * @Last Modified by: hexuan.zhang
+ * @Last Modified time: 2019-10-22 23:51:14
+ */
+
+/**
+* @param {number[]} nums
+* @param {number} target
+* @return {number}
+*/
+const threeSumClosest = (nums, target) => {
+
+};
