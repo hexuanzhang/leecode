bb8e3339466fbe29235cd5f6b99156e585a4ed25
hexuanzhang <136789658@qq.com>
2019-10-24 23:41:07 +0800

    17. Letter Combinations of a Phone Number v1

---
+++ JavaScript/17. Letter Combinations of a Phone Number.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合
+ * @Address: https://leetcode.com/problems/letter-combinations-of-a-phone-number/ | https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ * @Author: hexuan.zhang
+ * @Date: 2019-10-24 23:39:03
+ * @Last Modified by: hexuan.zhang
+ * @Last Modified time: 2019-10-24 23:40:47
+ */
+
+/**
+* @param {string} digits
+* @return {string[]}
+*/
+var letterCombinations = (digits) => {
+
+};
