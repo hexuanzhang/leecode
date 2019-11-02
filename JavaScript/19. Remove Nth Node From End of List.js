755d7d2ac39245c642c32338fff02e2d477dd254
hexuanzhang <136789658@qq.com>
2019-11-02 21:49:43 +0800

    /**

---
+++ JavaScript/19. Remove Nth Node From End of List.js
@@ -0,0 +1,24 @@
+/*
+ * @Description: 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点
+ * @Address: https://leetcode.com/problems/remove-nth-node-from-end-of-list/ | https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+ * @Author: hexuan.zhang
+ * @Date: 2019-11-02 21:47:39
+ * @Last Modified by: hexuan.zhang
+ * @Last Modified time: 2019-11-02 21:49:30
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const removeNthFromEnd = (head, n) => {
+
+};
