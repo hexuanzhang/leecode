b2983f0c095d5aef85a6d7e7405b6b596b08413c
hexuanzhang <136789658@qq.com>
2019-11-04 11:56:39 +0800

    19. Remove Nth Node From End of List v2

--- JavaScript/19. Remove Nth Node From End of List.js
+++ JavaScript/19. Remove Nth Node From End of List.js
@@ -4,7 +4,7 @@
  * @Author: hexuan.zhang
  * @Date: 2019-11-02 21:47:39
  * @Last Modified by: hexuan.zhang
- * @Last Modified time: 2019-11-02 21:49:30
+ * @Last Modified time: 2019-11-04 11:55:59
  */
 
 /**
@@ -15,10 +15,35 @@
  * }
  */
 /**
+ * 思路：删除倒数第 n 个节点需要将倒数第 n+1 个节点的 next 指向倒数第 n-1 个节点
+ *  1. 获取节点数量
+ *  2. 遍历节点，将指针指向倒数第 n+1 个节点
+ *  3. 删除倒数第 n 个节点
+ *
  * @param {ListNode} head
  * @param {number} n
  * @return {ListNode}
  */
 const removeNthFromEnd = (head, n) => {
+    const dummyNode = new ListNode('');
+    dummyNode.next = head;
 
+    // 统计节点数量
+    let count = 0,
+        firstNode = dummyNode.next;
+    while (!Object.is(firstNode, null)) {
+        count++;
+        firstNode = firstNode.next;
+    }
+
+    count -= n;
+    firstNode = dummyNode;
+    while (count > 0) { // 遍历节点，让 fisrtNode 指向倒数第 n+1 个节点
+        firstNode = firstNode.next;
+        count--;
+    }
+    // 删除倒数第 n 个节点
+    firstNode.next = firstNode.next.next;
+
+    return dummyNode.next;
 };
